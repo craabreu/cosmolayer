42b2cdeeb09952d1d787ea89445452381073ccf8
Charlles Abreu <craabreu@gmail.com>
2026-01-02 13:34:56 -0500

    Updated interaction matrix function

--- cosmolayer/__init__.py
+++ cosmolayer/__init__.py
@@ -3,10 +3,10 @@ Differentiable COSMO-Type Activity Coefficient Layer
 """
 
 from ._version import __version__
-from .sac import Component, create_lin_sandler_matrix
+from .sac import Component, create_cosmo_sac_2002_matrix
 
 __all__ = [
     "__version__",
     "Component",
-    "create_lin_sandler_matrix",
+    "create_cosmo_sac_2002_matrix",
 ]
--- cosmolayer/sac/__init__.py
+++ cosmolayer/sac/__init__.py
@@ -1,4 +1,4 @@
 from .component import Component
-from .interaction_matrix import create_lin_sandler_matrix
+from .interaction_matrix import create_cosmo_sac_2002_matrix
 
-__all__ = ["Component", "create_lin_sandler_matrix"]
+__all__ = ["Component", "create_cosmo_sac_2002_matrix"]
