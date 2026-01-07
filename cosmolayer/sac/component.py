0dd77a427b3cd9f4904ecbb41db0300f4b1ed650
Charlles Abreu <craabreu@gmail.com>
2026-01-07 17:01:38 -0500

    Add segment-type probability bar plot to Component docstring
    - Add plot showing segment-type probabilities as a bar chart
    - Uses get_log_probabilities() to demonstrate probability distribution
    - Placed in main Component class docstring for better visibility
    - Shows probabilities for all segment types (NHB, OH, OT combined)

--- cosmolayer/sac/component.py
+++ cosmolayer/sac/component.py
@@ -89,6 +89,23 @@ class Component:
         >>> _ = ax.set_ylabel("Surface area contribution (Å²)")
         >>> _ = ax.legend()
         >>> fig.tight_layout()
+
+    Plotting the segment-type probabilities:
+
+    .. plot::
+        :context: close-figs
+
+        >>> from importlib.resources import files
+        >>> from cosmolayer.sac import Component
+        >>> from matplotlib import pyplot as plt
+        >>> path = files("cosmolayer.data") / "C=C(N)O.cosmo"
+        >>> component = Component(path)
+        >>> fig, ax = plt.subplots(figsize=(8, 4))
+        >>> log_p = component.get_log_probabilities()
+        >>> _ = ax.bar(range(len(log_p)), np.exp(log_p))
+        >>> _ = ax.set_xlabel("Segment type index")
+        >>> _ = ax.set_ylabel("Probability")
+        >>> fig.tight_layout()
     """
 
     def __init__(  # noqa: PLR0913
